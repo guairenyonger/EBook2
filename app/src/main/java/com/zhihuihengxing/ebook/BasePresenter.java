d8fa63f9341c0683fa06236b69a83ae6e568713f
guairenyonger <huangyaoyongallen@163.com>
2017-09-13 15:58:42 +0800

    Revert "第一个版本"
    This reverts commit 9bb2e15cc88b80e76aaab047fe7379ca4cc97e5a.

--- app/src/main/java/com/zhihuihengxing/ebook/BasePresenter.java
+++
@@ -1,9 +0,0 @@
-package com.zhihuihengxing.ebook;
-
-/**
- * Created by Administrator on 2017/3/4 0004.
- */
-public interface BasePresenter {
-    void subscribe();
-    void unsubscribe();
-}
--- app/src/main/java/com/zhihuihengxing/ebook/BaseView.java
+++
@@ -1,8 +0,0 @@
-package com.zhihuihengxing.ebook;
-
-/**
- * Created by Administrator on 2017/3/4 0004.
- */
-public interface BaseView<T> {
-    void setPresenter(T presenter);
-}
